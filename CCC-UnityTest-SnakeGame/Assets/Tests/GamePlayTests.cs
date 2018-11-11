66cca5bbebf688844c7e3d693ed71945021b18ab
anasmatic <anasmatic@gmail.com>
2018-11-11 23:01:27 +0800

    Upgarde UnitTest to use TestRunner
    - Change Map tests from separate unit testing project to UnityTest TestRunner
    - add test case for object pooling

---
+++ CCC-UnityTest-SnakeGame/Assets/Tests/GamePlayTests.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class GamePlayTests {
+
+
+    [UnityTest]
+    public IEnumerator GamePlayTests_Pool_HasCreatedInstancesAsMuchAsPoolSize() {
+        //define pool size value
+        int poolSize = 21;
+        //create gameObject with Pool script attached to it
+        FruitsPool pool = new GameObject().AddComponent<FruitsPool>();
+        pool.ConstructForUnityTest(poolSize);
+        //init pool, with dummy data
+        pool.InitPool( new IObserver[]{} );
+        Debug.Log(poolSize+" vs "+ pool.pool.Count);
+        // Use the Assert class to test conditions.
+        Assert.AreEqual(poolSize, pool.pool.Count);
+        // yield to skip a frame
+        
+        yield return null;
+    }
+}
---
+++ CCC-UnityTest-SnakeGame/Assets/Tests/MapTests.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System;
+
+public class MapTests {
+
+    [Test]
+    public void MapTests_GetValidPosition_ReturnsPositionThatIsNotBlockedBySnake() {
+        int[][] map;
+        for (int i = 0; i < 200; i++)
+        {
+            //reset map
+            Map.Instance.InitMap(20, 20);
+            map = Map.Instance.GetMapNow();
+            //mimic a snake movement that blocks parts of the map, errr let's randomly block some cells in the map
+            RandomlyBlock(map);
+            //Map.Instance.PrintMap();
+            var pos = Map.Instance.GetValidPosition();
+            
+            float posVal = (float)map[(int)pos.x][(int)pos.z];
+            Assert.AreEqual(posVal, 0);
+        }
+    }
+
+    private void RandomlyBlock(int[][] map)
+    {
+        System.Random rand = new System.Random();
+        for (int i = 0; i < map.Length; i++){
+            for (int j = 0; j < map[i].Length; j++){
+                map[i][j] = rand.Next(0,2);//randome from 0 to 1
+            }
+        }
+    }
+}
