51e635e5d43f4506c998a94025a2b66b6b1ae8a7
Anas Metwally <anasmatic@gmail.com>
2018-11-11 23:21:22 +0800

    Rename Project Folder

--- UnitySampleProject-SnakeGame/Assets/Scripts/Game/Fruit/RedFruit.cs
+++ UnitySampleProject-SnakeGame/Assets/Scripts/Game/Fruit/RedFruit.cs
@@ -15,7 +15,7 @@ public class RedFruit : BaseFruit {
     // unique animation of the red fruit !
     public override void animate()
     {
-        transform.localPosition = startPos + new Vector3(0.0f, Mathf.Sin(Time.time)*0.5f, 0.0f);//up and down
+        //transform.localPosition = startPos + new Vector3(0.0f, Mathf.Sin(Time.time)*0.5f, 0.0f);//up and down
         transform.Rotate (0,0,45*Time.deltaTime);//rotation 
         base.animate();
     }
